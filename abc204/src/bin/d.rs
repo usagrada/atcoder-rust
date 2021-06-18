873fc0c6786c32272d2451a54795eeb8faa0e2cc
usa <usagrada@users.noreply.github.com>
2021-06-18 12:46:26 +0900

    ansAC 204D

--- abc204/src/bin/d.rs
+++ abc204/src/bin/d.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-  unimplemented!();
+  input! {
+    n: usize,
+    mut t: [usize; n],
+  }
+
+  let mut sum = 0;
+  for i in &t {
+    sum += i;
+  }
+
+  let mut dp = vec![vec![false; sum + 1]; n + 1];
+  dp[0][0] = true;
+
+  for i in 0..n {
+    for j in 0..=sum {
+      if j >= t[i] && dp[i][j - t[i]] {
+        dp[i + 1][j] = true;
+      } else if dp[i][j] {
+        dp[i + 1][j] = true;
+      }
+    }
+  }
+  for i in ((sum + 1) / 2)..=sum {
+    if dp[n][i] {
+      println!("{}", i);
+      break;
+    }
+  }
 }
